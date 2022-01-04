7e2c4d96877377ec1c85aff7947dd6dc2365dd4c
will-mei <williammei@126.com>
2022-01-04 13:59:06 +0800

    add vue3

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>hello, vue</h1>
+</template>
+
+<script setup></script>
+
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  purge: ["index.html"],
-  content: [],
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{vue,js}"],
+  // darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
