e680b40ab25608a95d5cc847c8be86d9d65e404a
will-mei <williammei@126.com>
2022-01-12 16:17:48 +0800

    import and use createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 // 引入router
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 // 引入路由页面配置
 import routes from './routes';
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
