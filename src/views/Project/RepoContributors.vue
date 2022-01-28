2f7c7f98e919f0d9d0a024188ef7b176b14089af
tujiapeng <tujiapeng_roc@163.com>
2022-01-28 15:22:39 +0800

    prepare contributor page

--- src/views/Project/RepoContributors.vue
+++ src/views/Project/RepoContributors.vue
@@ -1,6 +1,56 @@
 <template>
-    <h1>RepoContributors</h1>
+  <Crumbs />
+
+  <!-- branch filter  -->
+  <div class="flex h-20 bg-gray-50 border-y items-center gap-x-2 px-4">
+    <button
+      class="
+        min-w-fit
+        border
+        rounded-md
+        h-10
+        px-2
+        bg-white
+        hover:border-gray-400 hover:bg-gray-200
+      "
+    >
+      Development
+      <font-awesome-icon icon="angle-down" class="mx-2" />
+    </button>
+    <router-link
+      to="/projectOps/Project/RepoCommits"
+      class="
+        min-w-fit
+        px-3
+        py-2
+        border
+        rounded-md
+        bg-white
+        hover:bg-gray-200 hover:border-gray-400
+      "
+    >
+      History
+    </router-link>
+  </div>
+  <!-- end of branch filter  -->
+
+  <!-- commit charts -->
+  <div class="my-3">
+    <!-- page title  -->
+    <div>
+      <p class="text-3xl py-2">Commits to development</p>
+      <p class="text-lg text-gray-700">
+        Excluding merge commits. Limited to 6,000 commits.
+      </p>
+    </div>
+
+    <!-- charts  -->
+    <!-- end of charts  -->
+  </div>
+  <!-- end of comit charts -->
 </template>
 
-<script setup></script>
+<script setup>
+import Crumbs from "@/components/Crumbs.vue";
+</script>
 <style></style>
