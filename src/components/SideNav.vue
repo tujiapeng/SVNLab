8e78d8db910ce5e6ea84f5976e8cc364a067f675
tujiapeng <tujiapeng_roc@163.com>
2022-02-09 18:06:03 +0800

    style tweaks

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-full w-48 min-w-min overflow-y-scroll py-2 bg-gray-100">
+  <div class="h-full w-52 min-w-min overflow-y-scroll py-2 bg-gray-50">
     <!-- 导航栏标题 -->
     <div id="nav-title">
       <p
@@ -23,40 +23,30 @@
         <div>
           <!-- 菜单标题 -->
           <div
-            class="bg-indigo-100"
+            class="bg-indigo-100 hover:bg-indigo-200 py-2"
             @click="toggleCollapse('side-nav-sub-' + navItem.id)"
           >
-            <a
-              class="
-                px-1
-                pr-2
-                my-4
-                text-lg
-                font-semibold
-                text-gray-700
-                tracking-wide
-              "
-              >{{ navItem.title }}</a
-            >
-            <font-awesome-icon class="float-right mr-1" :icon="navItem.icon" />
+            <a class="pl-2 text-lg font-semibold text-gray-700 tracking-wide">{{
+              navItem.title
+            }}</a>
+            <font-awesome-icon class="float-right mr-1" icon="chevron-down" />
           </div>
           <!-- 子菜单 -->
-          <div :id="'side-nav-sub-' + navItem.id" style="display: none">
+          <div :id="'side-nav-sub-' + navItem.id">
             <ul v-for="subItem in navItem.sub_nav_items" :key="subItem.id">
               <!-- 子菜单项 -->
               <li
                 class="
-                  px-1
-                  py-1
-                  hover:border-l-4 hover:border-blue-400 hover:bg-blue-100
+                  py-2
+                  hover:border-l-4 hover:border-blue-400 hover:bg-blue-200
                   cursor-pointer
-                  text-gray-800
+                  text-gray-700
                   flex
                   items-center
                 "
               >
                 <router-link :to="subItem.link_to" class="w-full">
-                  <font-awesome-icon :icon="subItem.icon" />
+                  <font-awesome-icon :icon="subItem.icon" class="ml-6" />
                   <span class="pl-2">{{ subItem.name }}</span>
                 </router-link>
               </li>
@@ -67,30 +57,16 @@
       <!-- end of class1 nav -->
     </ul>
   </div>
-  <!-- test -->
-  <!-- <div>
-    <div>
-      <h2 @click="nInc">{{ count }} is {{ n }}</h2>
-    </div>
-  </div> -->
 </template>
 
 <script setup>
 defineProps(["navTree", "navTitle"]);
 
-// func test
-// import { ref } from "vue";
-// const count = ref("count");
-// const n = ref(18);
-
-// const nInc = () => {
-//   n.value++;
-// };
-const toggleCollapse = (name) => {
-  if (parent.document.getElementById(name).style.display) {
-    parent.document.getElementById(name).style.display = "";
+const toggleCollapse = (id) => {
+  if (parent.document.getElementById(id).style.display) {
+    parent.document.getElementById(id).style.display = "";
   } else {
-    parent.document.getElementById(name).style.display = "none";
+    parent.document.getElementById(id).style.display = "none";
   }
 };
 </script>
