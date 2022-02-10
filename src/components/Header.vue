a705f26a750e9a614e4a0f6ecae1922f0176051e
will-mei <williammei@126.com>
2022-02-10 16:49:11 +0800

    header style tweaks

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,38 +4,38 @@
   <!-- 菜单 -->
   <div
     id="header"
-    class="fixed w-full flex bg-gray-100 border-b-2 items-center text-lg"
+    class="fixed w-full flex bg-gray-100 border-b-2 items-end pt-3 text-lg"
   >
     <!-- logo  -->
-    <div id="site-logo" class="ml-3 my-2 mb-1">
+    <div id="site-logo" class="mx-3">
       <router-link to="/" class="text-4xl font-bold">SVNLab</router-link>
     </div>
 
     <!-- head bar -->
-    <div class="w-full flex justify-between ml-3 mt-1">
+    <div class="w-full flex justify-between ml-3">
       <!-- admin menu  -->
-      <div id="title-container" class="flex items-center gap-x-2">
+      <div id="title-container" class="flex font-semibold text-center">
         <router-link
           to="/projectOps"
-          class="px-2 rounded hover:bg-gray-300"
+          class="w-24 hover:border-b-2 hover:border-b-blue-600 hover:text-indigo-700"
         >
           Project
         </router-link>
-        <router-link to="/groups" class="px-2 rounded hover:bg-gray-300">
+        <router-link to="/groups" class="w-24 hover:border-b-2 hover:border-b-blue-600 hover:text-indigo-700">
           Groups
         </router-link>
-        <button type="button" class="px-2 rounded hover:bg-gray-300">
+        <div class="w-24 hover:border-b-2 hover:border-b-blue-600 hover:text-indigo-700">
           More
           <font-awesome-icon icon="angle-down" class="mx-1" />
-        </button>
-        <button type="button" class="px-2 rounded hover:bg-gray-300">
+        </div>
+        <div class="w-24 hover:border-b-2 hover:border-b-blue-600 hover:text-indigo-700">
           Admin
           <font-awesome-icon icon="tools" />
-        </button>
+        </div>
       </div>
 
       <!-- nav icons -->
-      <div id="navbar-collapse" class="flex text-gray-600 justify-end gap-x-1">
+      <div id="navbar-collapse" class="flex text-gray-600 justify-end gap-x-1 pb-1">
         <!-- new -->
         <button type="button" class="px-1 rounded hover:bg-gray-300">
           <font-awesome-icon icon="plus-square" class="mx-1" />
@@ -71,7 +71,7 @@
         <button
           type="button"
           class="
-            mr-3
+            mr-2
             px-1
             rounded-full
             w-28
