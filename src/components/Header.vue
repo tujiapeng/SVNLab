59b636445a9162c2e9aef7b5e5fc6151f0f3dc7d
will-mei <williammei@126.com>
2022-02-15 13:08:14 +0800

    pop menu test

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,11 +24,14 @@
         <router-link to="/groups" class="w-24 hover:border-b-2 hover:border-b-blue-600 hover:text-indigo-700">
           Groups
         </router-link>
-        <div class="w-24 hover:border-b-2 hover:border-b-blue-600 hover:text-indigo-700">
+        <div class="w-24 hover:border-b-2 hover:border-b-blue-600 hover:text-indigo-700 cursor-pointer" @click="switchDisplayById('menuOnMore')">
           More
           <font-awesome-icon icon="angle-down" class="mx-1" />
+          <div id="menuOnMore" style="display: none">
+            <MenuList :menuItems="m1" />
+          </div>
         </div>
-        <div class="w-24 hover:border-b-2 hover:border-b-blue-600 hover:text-indigo-700">
+        <div class="w-24 hover:border-b-2 hover:border-b-blue-600 hover:text-indigo-700 cursor-pointer">
           Admin
           <font-awesome-icon icon="tools" />
         </div>
@@ -37,9 +40,12 @@
       <!-- nav icons -->
       <div id="navbar-collapse" class="flex text-gray-600 justify-end gap-x-1 pb-1">
         <!-- new -->
-        <button type="button" class="px-1 rounded hover:bg-gray-300">
+        <button type="button" class="px-1 rounded hover:bg-gray-300" @click="switchDisplayById('menuOnAdd')">
           <font-awesome-icon icon="plus-square" class="mx-1" />
           <font-awesome-icon icon="angle-down" class="mx-1" />
+          <div id="menuOnAdd" style="display: none">
+            <MenuList :menuItems="m2" />
+          </div>
         </button>
         <!-- search  -->
         <input
@@ -64,8 +70,12 @@
           <font-awesome-icon icon="check-square" />
         </button>
         <!-- help -->
-        <button type="button" class="px-2 rounded hover:bg-gray-300">
+        <button type="button" class="px-2 rounded hover:bg-gray-300" @click="switchDisplayById('menuOnHelp')">
           <font-awesome-icon icon="question-circle" />
+          <font-awesome-icon icon="angle-down" class="mx-1" />
+          <div id="menuOnHelp" style="display: none" class="-ml-20">
+            <MenuList :menuItems="m2" />
+          </div>
         </button>
         <!-- user avatar  -->
         <button
@@ -89,6 +99,46 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import MenuList from "@/components/MenuList.vue";
+
+const m1 = [
+  {
+    title: "activity",
+    url: "#"
+  },
+  {
+    title: "menu1 t2",
+    url: "#"
+  }
+]
+
+const m2 = [
+  {
+    title: "menu2 text1",
+    url: "#"
+  },
+  {
+    title: "menu2 text2",
+    url: "#"
+  },
+  {
+    title: "menu2 text3",
+    url: "#"
+  },
+  {
+    title: "menu2 text4",
+    url: "#"
+  }
+]
+
+const switchDisplayById = (id) => {
+  if (parent.document.getElementById(id).style.display) {
+    parent.document.getElementById(id).style.display = "";
+  } else {
+    parent.document.getElementById(id).style.display = "none";
+  }
+}
+</script>
 
 <style></style>
---
+++ src/components/MenuList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="absolute mt-3 text-gray-800 font-normal">
+    <ul class="bg-white rounded-md border py-2 divide-y">
+      <li v-for="mItem in menuItems" key="mItem.title" class="px-3 py-1 hover:bg-gray-200">
+        <a :href="mItem.url">{{ mItem.title }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+defineProps(["menuItems"])
+</script>
+
+<style></style>
